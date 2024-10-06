f2336b8e0e2e0f7f57bfce0de824cc74e24a2ca0
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-10-06 18:25:17 +0000

    fix: remove unused code in input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -73,15 +73,3 @@ function Input({ locationText, handleSuggestion, handleLocationChange, handleGue
 }
 
 export default Input;
-
-/*
-    // minus
-    <svg width="24px" height="24px" viewBox="0 0 24 24">
-        <path fill="#000" d="M19,13H5V11H19V13Z" />
-    </svg>
-
-    // plus
-    <svg width="24px" height="24px" viewBox="0 0 24 24">
-        <path fill="#000" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
-    </svg>
-*/
